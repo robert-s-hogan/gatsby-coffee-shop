61566a82958694fbeacc20b8ba365e12075c648d
Robert Hogan <rhogan7@gmail.com>
2021-09-08 15:43:08 -0700

    Added /posts and MDX support.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,29 @@ module.exports = {
         description: "Here is the default starter multi-page",
         image: "https://res.cloudinary.com/dmfdjwwgb/image/upload/v1630950348/robertshogan.com/J_R___T_8.21.21-31_copy_k9zia3.jpg",
     },
-    plugins: ["gatsby-plugin-sitemap", "gatsby-plugin-react-helmet"],
+    plugins: [
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "posts",
+                path: `${__dirname}/src/posts`,
+            },
+        },
+        {
+            resolve: "gatsby-plugin-page-creator",
+            options: {
+                path: `${__dirname}/src/posts`,
+            },
+        },
+        {
+            resolve: "gatsby-plugin-mdx",
+            options: {
+                defaultLayouts: {
+                    posts: require.resolve("./src/components/layout.js"),
+                },
+            },
+        },
+    ],
 };
