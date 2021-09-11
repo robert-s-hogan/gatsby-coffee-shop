ae4989377fce46e07aea929f733ce1b6fe2a7569
Robert Hogan <rhogan7@gmail.com>
2021-09-10 17:52:31 -0700

    Added GatsbyImage config on about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,25 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
+import { getImage, GatsbyImage } from "gatsby-plugin-image";
 import { Layout } from "../components/layout.js";
 
-export default function About() {
+export const query = graphql`
+    query MyQuery {
+        file(name: { eq: "icon" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: DOMINANT_COLOR)
+            }
+        }
+    }
+`;
+
+export default function About({ data }) {
     return (
         <Layout
             title="About This Site"
             description="More information about this about page."
         >
+            <GatsbyImage image={getImage(data.file)} alt="Gatsby Icon" />
             <h1>About This Site</h1>
             <Link to="/">Back to Home</Link>
         </Layout>
