2c408fc581af047c4fb23bd1df5bb16f9a456e76
Robert Hogan <rhogan7@gmail.com>
2021-11-05 17:01:34 -0700

    Gatsby-config: removed gatsby sanity plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,12 +56,5 @@ module.exports = {
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
         "gatsby-transformer-sharp",
-        // {
-        //     resolve: "gatsby-source-sanity",
-        //     options: {
-        //         projectId: "vnkupgyb",
-        //         dataset: "production",
-        //     },
-        // },
     ],
 };
