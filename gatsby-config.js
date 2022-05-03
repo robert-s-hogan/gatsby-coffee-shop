bca9cebc61fafe1d0b7d56189fe44d205ff11f17
Robert Hogan <rhogan7@gmail.com>
2022-05-03 08:13:13 -0700

    Updated build with sitemap on new domain.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,62 +1,63 @@
 require("dotenv").config();
 
 module.exports = {
-    siteMetadata: {
-        siteUrl: "https://defaultmultipagegatsbystarterm.gatsbyjs.io/",
-        title: "Local Coffee Shop",
-        description:
-            "Here is a template for local coffee shops to have a clean, user experience, and fast professional created site.",
-        image: "https://res.cloudinary.com/dmfdjwwgb/image/upload/v1630950348/robertshogan.com/J_R___T_8.21.21-31_copy_k9zia3.jpg",
+  siteMetadata: {
+    siteUrl: "https://www.localcoffeeshop.page/",
+    title: "Local Coffee Shop",
+    description:
+      "Here is a template for local coffee shops to have a clean, user experience, and fast professional created site.",
+    image:
+      "https://www.localcoffeeshop.page/static/78e3d8ed986a3aa5dd73278bdf2348a7/f8f67/coffee_hero.webp",
+  },
+  plugins: [
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.localcoffeeshop.page/`,
+      },
     },
-    plugins: [
-        "gatsby-plugin-sitemap",
-        "gatsby-plugin-react-helmet",
-        `gatsby-plugin-postcss`,
-        {
-            resolve: `gatsby-plugin-canonical-urls`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    "gatsby-remark-images",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
             options: {
-                siteUrl: `https://defaultmultipagegatsbystarterm.gatsbyjs.io/`,
+              maxWidth: 1200,
             },
+          },
+        ],
+        defaultLayouts: {
+          posts: require.resolve("./src/components/posts-layout.js"),
         },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "posts",
-                path: `${__dirname}/src/posts`,
-            },
-        },
-        {
-            resolve: "gatsby-plugin-page-creator",
-            options: {
-                path: `${__dirname}/src/posts`,
-            },
-        },
-        "gatsby-remark-images",
-        {
-            resolve: "gatsby-plugin-mdx",
-            options: {
-                gatsbyRemarkPlugins: [
-                    {
-                        resolve: "gatsby-remark-images",
-                        options: {
-                            maxWidth: 1200,
-                        },
-                    },
-                ],
-                defaultLayouts: {
-                    posts: require.resolve("./src/components/posts-layout.js"),
-                },
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "images",
-                path: `${__dirname}/src/images`,
-            },
-        },
-        "gatsby-plugin-image",
-        "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
-    ],
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+  ],
 };
